a9432a2c46bf71474825e2d71d95e74a3c8a8d64
krluss <karol.kurcz1337@gmail.com>
2022-09-18 11:45:32 +0200

    environment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCUZ_kuROgE8aIzzQyC48hebQAtgTray94",
+    authDomain: "crud-15b66.firebaseapp.com",
+    projectId: "crud-15b66",
+    storageBucket: "crud-15b66.appspot.com",
+    messagingSenderId: "56475992427",
+    appId: "1:56475992427:web:90960222f6327b55040c2e"
+  },
 };
